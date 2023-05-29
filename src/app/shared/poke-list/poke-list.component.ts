c3d09588f9fb446a272d065e016e3af6ed6bdb42
Rubensherculano <rubensherculano@hotmail.com>
2023-05-29 11:00:34 -0300

    Finalização pokedex

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,25 +1,39 @@
 import { PokeApiService } from './../../service/poke-api.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit{
 
   public getAllPokemons: any;
+  private setAllPokemons: any;
+
+  public apiError: boolean = false;
 
   constructor(
     private pokeApiService: PokeApiService
   ) {  }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe(
-      res => {
-        this.getAllPokemons = res.results;
-        console.log(this.getAllPokemons);
+    this.pokeApiService.apiListAllPokemons.subscribe({
+      next: (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      error: (err) => {
+        this.apiError = true;
       }
-    );
+    });
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter( (res: any) => {
+      return !res.name.indexOf(value.toLowerCase())
+    });
+
+    this.getAllPokemons = filter;
   }
 }
